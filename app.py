30c48d54972af680121832d54c461a4cfb2708e6
kirby ellingson <hkellingson@gmail.com>
2022-06-29 21:38:37 -0500

    make more thing

--- app.py
+++ app.py
@@ -1,13 +1,2 @@
-from flask import Flask
-app = Flask(__name__)
-# index route
-
-
-@app.route('/')
-def index():
-    return 'Hellow, this is PetFax!'
-
-
-@app.route('/pets')
-def pets():
-    return 'These are our pets available for adopotion!'
+from petfax import create_app
+app = create_app()
--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+# factory
+
 
 def create_app():
     app = Flask(__name__)
@@ -7,5 +9,8 @@ def create_app():
     @app.route('/')
     def hello():
         return 'Howdy Howdy Howdy'
+    # register pet Blueprint
+    from . import pets
+    app.register_blueprint(pet.bp)
 
     return app
